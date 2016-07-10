510181d19fddb11ca5b6b6ea2f8fa6d66e1f898d
Sairanganath Vemuri <sairanganath.v@nearbuy.com>
2016-07-11 03:49:17 +0530

    Corrected ls statement

--- modular_ETL/load_scripts/load_deal.sh
+++ modular_ETL/load_scripts/load_deal.sh
@@ -187,10 +187,7 @@ rm "$v_data_object"_inc_table_result.txt
 #-X-#-X-#-X-#-X-#-X-#-X-#-X-#-X-#-X-#-X-#-X-#-X-#-X-#-X-#-X-#-X-#-X-#-X-#-X-#-X-#-X-#-X-#
 
 if [[ "`bq ls $v_dataset_name | awk '{print $1}' | grep \"\b$tableName\b\"`" == "$tableName" ]] ;
-    then
-    then
-	then bq rm $v_destination_tbl;
-fi 
+    then 
 
         ## Make another table with prior (till last run) data 
         v_query="SELECT * FROM $v_dataset_name.$tableName WHERE _id NOT IN (SELECT _id FROM $v_metadataset_name.incremental_$tableName)";

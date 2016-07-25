afb5f23f38bea82a045f3ab35e8a00d83a5409b7
Sairanganath Vemuri <sairanganath.v@nearbuy.com>
2016-07-26 00:20:25 +0530

    Corrected the maxBadRecords to 10

--- modular_ETL/load_scripts/load_ratings_and_reviews.sh
+++ modular_ETL/load_scripts/load_ratings_and_reviews.sh
@@ -25,7 +25,7 @@ v_task_datetime=`echo $(date -d "@$v_task_start_epoch" +"%Y-%m-%d_%H:%M_%Z")`;
 v_etl_task='load'
 
 schemaFileName=schema_ratings_and_reviews.json
-maxBadRecords=0
+maxBadRecords=10
 
 v_data_object=$1;
 tableName=$1;

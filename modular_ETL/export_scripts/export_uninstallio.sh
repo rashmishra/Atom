f7fed3028da3c109124c4534232a4e2df0dea464
Rashmi Mishra <rashmimishra@GODEL0495.local>
2016-08-09 16:56:33 +0530

    updated export_uninstallio

--- modular_ETL/export_scripts/export_uninstallio.sh
+++ modular_ETL/export_scripts/export_uninstallio.sh
@@ -117,7 +117,7 @@ v_subtask="CSV export";
 p_exit_upon_error "$v_task_status" "$v_subtask"
 
 # Zipping the exported data file
-gzip -f $v_data_dump_dir/$v_data_object.csv.gz
+
 
 ###################################################################################
 ## Storing the status (success/failed) into respective text file. This will be in

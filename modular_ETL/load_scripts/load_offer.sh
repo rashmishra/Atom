3fd9a27676959822193217fb794bf4bf5df6ab7f
Sairanganath Vemuri <sairanganath.v@nearbuy.com>
2016-10-07 18:06:27 +0530

    Restricted # of o/p records while executing 'bq query'

--- modular_ETL/load_scripts/load_offer.sh
+++ modular_ETL/load_scripts/load_offer.sh
@@ -193,7 +193,7 @@ if [[ "`bq ls $v_dataset_name | awk '{print $1}' | grep \"\b$tableName\b\"`" == 
         v_query="SELECT * FROM $v_dataset_name.$tableName WHERE _id NOT IN (SELECT _id FROM $v_metadataset_name.incremental_$tableName)";
         v_destination_tbl="$v_metadataset_name.prior_$tableName";
         echo "Destination table is $v_destination_tbl and Query is $v_query"
-        bq query  --maximum_billing_tier 10 --allow_large_results=1  --quiet --flatten_results=0 --replace --destination_table=$v_destination_tbl "$v_query" 2> "$v_data_object"_prior_table_result.txt &
+        bq query  --maximum_billing_tier 10 --allow_large_results=1 -n 0 --quiet --flatten_results=0 --replace --destination_table=$v_destination_tbl "$v_query" 2> "$v_data_object"_prior_table_result.txt &
         v_pid=$!
 
         wait $v_pid
@@ -227,7 +227,7 @@ fi
 
 v_destination_tbl="$v_metadataset_name.prior_$tableName";
 v_query="SELECT * FROM $v_metadataset_name.incremental_$tableName";
-bq query --append=1 --flatten_results=0 --allow_large_results=1 --destination_table=$v_destination_tbl "$v_query" 2> "$v_data_object"_table_union_result.txt &
+bq query --append=1 --flatten_results=0 --allow_large_results=1 -n 0 --destination_table=$v_destination_tbl "$v_query" 2> "$v_data_object"_table_union_result.txt &
 v_pid=$!
 
 wait $v_pid

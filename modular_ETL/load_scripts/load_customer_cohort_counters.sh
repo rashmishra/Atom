f19151b8302eecae365301fe6f3ac8885568d46f
Sairanganath Vemuri <sairanganath.v@nearbuy.com>
2016-11-27 01:25:08 +0530

    CSV to JSON 2

--- modular_ETL/load_scripts/load_customer_cohort_counters.sh
+++ modular_ETL/load_scripts/load_customer_cohort_counters.sh
@@ -163,7 +163,7 @@ echo "Schema File path is: $v_schema_filepath"
 
 # Loading the data directly
 v_destination_tbl="$v_dataset_name.${tableName}";
-bq load --replace --quiet --field_delimiter=',' --source_format=CSV --skip_leading_rows=1 --max_bad_records=$maxBadRecords --allow_jagged_rows=1 --allow_quoted_newlines=1 --ignore_unknown_values=1 $v_destination_tbl $v_cloud_storage_path/$v_fileName $v_schema_filepath/$schemaFileName 2> "$v_data_object"_inc_table_result.txt
+bq load --quiet --source_format=NEWLINE_DELIMITED_JSON --replace --ignore_unknown_values=1 --max_bad_records=$maxBadRecords $v_destination_tbl $v_cloud_storage_path/$v_fileName $v_schema_filepath/$schemaFileName 2> "$v_data_object"_inc_table_result.txt
 #2> "$v_data_object"_final_table_result.txt
 v_pid=$!
 

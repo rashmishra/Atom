d948dae7871a4a6f51a1ec2058042e847a003bd8
Sairanganath Vemuri <sairanganath.v@nearbuy.com>
2017-03-03 01:42:07 +0530

    Changed the Database name DBNAME to 'oms_v2' from 'oms'.

--- modular_ETL/export_scripts/export_order_line_event.sh
+++ modular_ETL/export_scripts/export_order_line_event.sh
@@ -90,7 +90,7 @@ p_exit_upon_error(){
 DBHOST=nb-prod-oms-db-ugd.c6vqep7kcqpl.ap-southeast-1.rds.amazonaws.com
 # DBHOST=nb-prod-oms-db-ugd-read.c6vqep7kcqpl.ap-southeast-1.rds.amazonaws.com
 DBPORT=5432
-DBNAME=oms
+DBNAME=oms_v2
 DBUSER=oms
 DBPASS=0mspr0d$
 DATE=`date +%Y-%m-%d`

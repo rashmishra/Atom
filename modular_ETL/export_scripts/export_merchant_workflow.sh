200706aae26f1f220e3f9b8e8b8d888a9598cc12
Nitesh Kumar <nitesh.kumar@nearbuy.com>
2017-12-14 18:59:46 +0530

    export script changed for prod

--- modular_ETL/export_scripts/export_merchant_workflow.sh
+++ modular_ETL/export_scripts/export_merchant_workflow.sh
@@ -87,19 +87,19 @@ p_exit_upon_error(){
 
 cd $v_mongo_dir
 
-# v_arbiter_ip="10.20.1.190"
+v_arbiter_ip="10.20.1.190"
 
-# v_primary_ip=`./mongo   --host $v_arbiter_ip:27017 --eval="printjson(rs.isMaster())" | tail -n+3 | grep -v ISODate | grep -v "Object" | jq .primary`;
-# v_secondary_ip=`./mongo   --host $v_arbiter_ip --eval="printjson(rs.isMaster())" | tail -n+3 | grep -v ISODate | grep -v "Object" | jq .hosts | grep -v "$v_primary_ip" | grep -v  "\[" | grep -v "\]" | sed -e 's/\"//g' | sed -e 's/\ //g'`;
-# v_secondary_ip=`echo $v_secondary_ip | sed -e 's/,//g' | sed -e 's/-/./g' | sed -e 's/ip.//g' | head -n 1`;
+v_primary_ip=`./mongo   --host $v_arbiter_ip:27017 --eval="printjson(rs.isMaster())" | tail -n+3 | grep -v ISODate | grep -v "Object" | jq .primary`;
+v_secondary_ip=`./mongo   --host $v_arbiter_ip --eval="printjson(rs.isMaster())" | tail -n+3 | grep -v ISODate | grep -v "Object" | jq .hosts | grep -v "$v_primary_ip" | grep -v  "\[" | grep -v "\]" | sed -e 's/\"//g' | sed -e 's/\ //g'`;
+v_secondary_ip=`echo $v_secondary_ip | sed -e 's/,//g' | sed -e 's/-/./g' | sed -e 's/ip.//g' | head -n 1`;
 
-v_secondary_ip="nb-qa-db-groupon_fox.nbtools.com";
+#v_secondary_ip="nb-qa-db-groupon_fox.nbtools.com";
 
 echo "${v_data_object}: Secondary IP is $v_secondary_ip";
 
 
 
-./mongoexport --host "$v_secondary_ip" -u nbqa -p nearbuy --db groupon_fox  -c merchant_workflow --out $v_data_dump_dir/$v_data_object.json 2> $v_temp_dir/"$v_data_object"_extract_command_output.txt &
+./mongoexport --host "$v_secondary_ip" --db groupon_fox  -c merchant_workflow --out $v_data_dump_dir/$v_data_object.json 2> $v_temp_dir/"$v_data_object"_extract_command_output.txt &
 v_extract_pid=$!
 
 # Waiting for the process to complete

74b8a1918416dfd0bcdd5513de67497b23c5a261
Pratik Saxena <pratik.saxena@nearbuy.com>
2018-10-10 19:16:02 +0530

    changed sed location

--- modular_ETL/export_scripts/export_message.sh
+++ modular_ETL/export_scripts/export_message.sh
@@ -109,16 +109,13 @@ v_extract_pid=$!
 if wait $v_extract_pid; then
     echo "Process $v_extract_pid Status: success";
     v_task_status="success";
-    # Modifying format of mongo export to be same as version 2.4.9 required for atlas migration
-    sed -i -e 's/{"$numberLong":"\([0-9]*\)"}/\1/g' $v_data_dump_dir/$v_data_object.json
 else 
     echo "Process $v_extract_pid Status: failed";
     v_task_status="failed";
 fi
 
-
-
-
+# Modifying format of mongo export to be same as version 2.4.9 required for atlas migration
+sed -i -e 's/{"$numberLong":"\([0-9]*\)"}/\1/g' $v_data_dump_dir/$v_data_object.json
 
 v_log_obj_txt+=`echo "\n$(date) $v_task_status is the task status. \n"`;
 
